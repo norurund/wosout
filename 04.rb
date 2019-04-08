a54ceb65d3560d3fdf3e85dc675b0d9cd5109c10
norurund <fj1160ic@aa.jp.fujitsu.com>
2019-04-08 13:31:45 +0900

    改良

--- 04.rb
+++ 04.rb
@@ -1,3 +1,5 @@
+#パラメータファイル置き換え
+puts "日付、移管先、回数、住所コード、移管元"
 date = gets.chomp! #日付
 storeid = gets #移管先店舗
 t = gets.to_i
@@ -10,7 +12,7 @@ end
 l.each do |i|
     i.chomp!
     storeid.chomp!
-     @n << "\"#{storeid},#{i}\""
+     @n << "\"#{storeid}\",\"#{i}\""
 end
 fromid = gets.chomp! #移管元店舗
 f = File.open("EXCHANGE_ADDRESS_tmprate.txt","r")
@@ -18,18 +20,21 @@ buffer = f.read();
 buffer.gsub!(/from/,"#{fromid}")
 f = File.open("EXCHANGE_ADDRESS_tmprate.txt","w")
 f.write(buffer)
-f.close()
-File.open("EXCHANGE_ADDRESS_tmprate.txt","a") do |text|
  @n.each do |v|
-    text.puts v
+    f.puts v
  end
+f.close()
+
+File.open("EXCHANGE_ADDRESS_tmprate.txt","a") do |text|
 file = File.open("EXCHANGE_ADDRESS_tmprate.txt","r")
 lines = file.readlines
 file.close
+
 #ベツファイルに出力(同DIR)
 file = File.open("EXCHANGE_ADDRESS_#{date}_#{fromid}TO#{storeid}.txt","w")
 file.write(lines.join)
 text.close()
+
 #ファイル名変更
 #File.rename('bar.txt', 'EXCHANGE_ADDRESS_tmprate.txt')
 end
