9b341be7b4d0086e539be68d7af037dad65e9126
norurund <fj1160ic@aa.jp.fujitsu.com>
2019-04-22 14:19:08 +0900

    20190422修正

---
+++ ym503.sh
@@ -0,0 +1,46 @@
+#!/bin/sh 
+export PATH=$PATH:/usr/sbin
+TODAY=`date +%Y%m%d`
+ANS=`sudo grep -c 'HTTP/1.1" 503' /var/log/httpd/www.foobar.com_access_log_${TODAY}.log`
+MAIL_TO="voofoo@foobar.com"
+MAIL_FROM="test@foobar.com"
+SUBJECT="メールテスト"
+
+#503があるばあい
+mail_send () {
+
+cat << EOD | nkf -j -m0 | sendmail -t
+From: ${MAIL_FROM}
+To: ${MAIL_TO}
+Subject: ${SUBJECT}
+MIME-Version: 1.0
+Content-Type: text/plain; charset="ISO-2022-JP"
+Content-Transfer-Encoding: 7bit
+
+
+本日は${ANS}件です。
+
+EOD
+}
+#503が0件のばあい
+zeromail_send () {
+
+cat << EOD | nkf -j -m0 | sendmail -t
+From: ${MAIL_FROM}
+To: ${MAIL_TO}
+Subject: ${SUBJECT}
+MIME-Version: 1.0
+Content-Type: text/plain; charset="ISO-2022-JP"
+Content-Transfer-Encoding: 7bit
+
+
+本日はございません。
+
+EOD
+}
+if [ ${ANS} -eq 0 ]; then
+zeromail_send
+else
+mail_send
+fi
+exit 0
