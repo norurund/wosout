eb11561251d381adf60a690dd4a40cf50c83546f
myapp01 <curyybrednono@gmail.com>
2019-06-18 13:02:51 +0900

    Update por.py

--- por.py
+++ por.py
@@ -2,21 +2,27 @@ import sys
 ni = list(map(str,sys.stdin.readline().split()))
 ni.reverse()
 stack = []
-operator = {
+
+#四則演算処理
+formula = {
         '+': (lambda x, y: y + x),
         '-': (lambda x, y: y - x),
         '*': (lambda x, y: y * x),
         '/': (lambda x, y: float(y) / x)
 }
+
+#入力値に計算記号があるかどうか
 for index, z  in enumerate(ni):
     print(stack)
-    if z not in operator.keys():
+    if z not in formula.keys():
       stack.append(int(z))
       continue
     y = stack.pop()
     x = stack.pop()
-    stack.append(operator[z](x, y))
+    stack.append(formula[z](x, y))
     print('%s %s %s =' % (y, z, x))
+    
+#小数点で解を出す
 if stack[0] >= 1:
   print(int(stack[0]))
 else:
